1a0b143bf68726d1e3eace7c5a1743f3d11275d2
Ilya Loshkarev <loshkarev.i@gmail.com>
2019-10-24 21:26:02 +0300

    update with factors

--- Program.cs
+++ Program.cs
@@ -6,22 +6,32 @@ namespace BigInt
     {
         static void Main(string[] args)
         {
-            BigInt i = new BigInt(10000000);
+            BigInt i = new BigInt(132);
             BigInt j = new BigInt(999999999);
             BigInt k = new BigInt("1234567890123456798");
 
             Console.WriteLine(i);
             Console.WriteLine(j);
             Console.WriteLine(k);
-            
+
             Console.WriteLine(k + j);
             Console.WriteLine(j * j);
-            Console.WriteLine(j / 999);
+            Console.WriteLine(j / new BigInt(81));
             Console.WriteLine(1000 % i);
+            Console.WriteLine(j.Sqrt());
 
             Console.WriteLine(k / j);
             Console.WriteLine(k % j);
             Console.WriteLine((k / j) * j + (k % j) == k);
+
+            BigInt prod = 1;
+            foreach (BigInt val in j.Factors())
+            {
+                Console.Write($"{val}, ");
+                prod *= val;
+            }
+            Console.WriteLine(prod);
+
         }
     }
 }
